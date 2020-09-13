6203077c1e07a6ca0b5f4bf37ca3f70a86ab6d55
Nate Spilman <nate.spilman@gmail.com>
2020-09-12 18:02:14 -0700

    updating metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Camp Bos Collective`,
+    description: `A COLLECTIVE OF ARTIST, BEAT MAKERS, DESIGNERS, COMICS, AND CREATIVES.`,
+    author: `Camp Nos`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
