2ea9abdd907490cf83a8f8b5b3ec5d943cfefc54
Nate Spilman <nate.spilman@gmail.com>
2020-09-22 22:57:14 -0700

    adding static/assets to filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,6 +75,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       }
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
